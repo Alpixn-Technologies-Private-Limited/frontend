0b720e1f18dd2098ef10b4d3aa56da902071d5fb
Abhishek-Alpixn <abhishek_ptm@alpixn.com>
2025-07-29 17:52:07 +0530

    created themecontext for dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { FaBell, FaSearch, FaSun } from "react-icons/fa";
+import { CiDark } from "react-icons/ci";
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useTheme } from "../context/ThemeContext";
 
 const Navbar = () => {
+    const { theme, toggleTheme } = useTheme();
     return (
         <div className="w-full bg-white border-b border-gray-300 shadow px-6 py-4 flex items-center justify-between">
             <div className="flex items-center space-x-4">
@@ -26,9 +31,9 @@ const Navbar = () => {
                     <FaBell />
                 </span>
 
-                <span className="text-gray-600 text-xl cursor-pointer hover:text-yellow-500 transition duration-200">
-                    <FaSun />
-                </span>
+                <button onClick={toggleTheme} className="text-gray-600 text-xl cursor-pointer  transition duration-200">
+                 {theme=='light'?  <MdOutlineLightMode />:<MdOutlineDarkMode/> }  
+                </button>
 
                 <div className="flex items-center bg-gray-100 rounded-full px-4 py-2">
                     <FaSearch className="text-gray-500 mr-2" />
